986ef056ae873607fa8cdd110b98f4df39f74f61
YooJaeHyeok <yoojh5685@gmail.com>
2025-08-04 01:36:33 +0900

    [UI] 온보딩 페이지 UI

--- Tourding_FE/Views/Login/OnboardingPageView.swift
+++ Tourding_FE/Views/Login/OnboardingPageView.swift
@@ -5,22 +5,28 @@
 //  Created by 유재혁 on 8/3/25.
 //
 
+import Foundation
+import SwiftUI
+
 struct OnboardingPageView: View {
     let index: Int
     let page: OnboardingPage
 
     var body: some View {
-        VStack(spacing: 20) {
+        VStack(spacing: 0) {
             ForEach(page.image_sub, id: \.self) { imageName in
                 Image(imageName)
                     .resizable()
                     .scaledToFit()
                     .frame(height: 50)
+                    .padding(.bottom, 12)
             }
 
             Text(page.title)
                 .font(index == 0 ? .pretendardMedium(size: 18) : .pretendardSemiBold(size: 26))
                 .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.bottom, index == 0 ? 12 : 23)
 
             ForEach(page.images, id: \.self) { imageName in
                 Image(imageName)
@@ -31,3 +37,4 @@ struct OnboardingPageView: View {
         }
     }
 }
+
