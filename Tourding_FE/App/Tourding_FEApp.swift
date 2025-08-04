44680a71c3e1ef263173731a13abfe0ef642c64c
YooJaeHyeok <yoojh5685@gmail.com>
2025-08-05 03:23:39 +0900

    [Chore] 이미지 이름 변경

--- Tourding_FE/App/Tourding_FEApp.swift
+++ Tourding_FE/App/Tourding_FEApp.swift
@@ -14,8 +14,8 @@ import KakaoSDKUser
 @main
 struct Tourding_FEApp: App {
     @StateObject private var navigationManager = NavigationManager()
+    @StateObject private var loginViewModel = LoginViewModel()  // ✅ ViewModel 생성
     @State private var showSplash = true
-    @State private var isLoggedIn = false  // ✅ 로그인 상태 관리
     
     init() {
         // kakao sdk 초기화
@@ -31,60 +31,62 @@ struct Tourding_FEApp: App {
         
         
         WindowGroup {
-            if showSplash {
-                SplashView()
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                            loadKakaoToken { success in
-                                withAnimation {
-                                    isLoggedIn = success
-                                    showSplash = false
-                                }
-                                if !isLoggedIn {
-//                                    navigationManager.push(.LoginView)
+                if showSplash {
+                    SplashView()
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                loadKakaoToken { success in
+                                    withAnimation {
+                                        loginViewModel.isLoggedIn = success
+                                        showSplash = false
+                                    }
+                                    if !loginViewModel.isLoggedIn {
+                                        //                                    navigationManager.push(.LoginView)
+                                    }
                                 }
                             }
+                        } // : onAppear
+                        .onOpenURL { url in
+                            if AuthApi.isKakaoTalkLoginUrl(url) {
+                                _ = AuthController.handleOpenUrl(url: url)
+                            }
                         }
-                    } // : onAppear
-                    .onOpenURL { url in
-                        if AuthApi.isKakaoTalkLoginUrl(url) {
-                            _ = AuthController.handleOpenUrl(url: url)
-                        }
-                    }
-                
-            } else {
-                if isLoggedIn{
-                    NavigationStack(path: $navigationManager.path) {
-                        TabContentView(viewModel: viewModels)
-                            .navigationDestination(for: ViewType.self) { path in
-                                switch path{
-                                    // case 추가해서 탭뷰 제외 뷰 넣으면 됨
-                                case .LoginView:
-                                    LoginView(isLoggedIn: $isLoggedIn)
-
-                                default :
-                                    EmptyView()
-                                }
-                            } // : navigationDestination
-                    } // : NavigationStack
-                    .environmentObject(navigationManager)
-                    .onOpenURL { url in
-                        if AuthApi.isKakaoTalkLoginUrl(url) {
-                            _ = AuthController.handleOpenUrl(url: url)
-                        }
-                    }
+                    
                 } else {
-                    NavigationStack(path: $navigationManager.path) {
-                        LoginView(isLoggedIn: $isLoggedIn)
-                    }
-                    .environmentObject(navigationManager)
-                    .onOpenURL { url in
-                        if AuthApi.isKakaoTalkLoginUrl(url) {
-                            _ = AuthController.handleOpenUrl(url: url)
+                    if loginViewModel.isLoggedIn{
+                        NavigationStack(path: $navigationManager.path) {
+                            TabContentView(viewModel: viewModels)
+                                .navigationDestination(for: ViewType.self) { path in
+                                    switch path{
+                                        // case 추가해서 탭뷰 제외 뷰 넣으면 됨
+                                    case .LoginView:
+                                        LoginView()
+                                        
+                                    default :
+                                        EmptyView()
+                                    }
+                                } // : navigationDestination
+                        } // : NavigationStack
+                        .environmentObject(navigationManager)
+                        .environmentObject(loginViewModel)  //  여기서 주입
+                        .onOpenURL { url in
+                            if AuthApi.isKakaoTalkLoginUrl(url) {
+                                _ = AuthController.handleOpenUrl(url: url)
+                            }
                         }
-                    }
-                }// : if-else
-            }
+                    } else {
+                        NavigationStack(path: $navigationManager.path) {
+                            LoginView()
+                        }
+                        .environmentObject(navigationManager)
+                        .environmentObject(loginViewModel)  //  여기서 주입
+                        .onOpenURL { url in
+                            if AuthApi.isKakaoTalkLoginUrl(url) {
+                                _ = AuthController.handleOpenUrl(url: url)
+                            }
+                        }
+                    }// : if-else
+                }
         }
     }
 }
