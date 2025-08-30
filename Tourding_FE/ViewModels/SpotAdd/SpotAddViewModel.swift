cb3014d2ee48e18a6200736f0205f7fae892d494
LYH513 <peperony367@gmail.com>
2025-08-30 16:37:23 +0900

    [UI]: 스팟추가 UI 1차 완료

--- Tourding_FE/ViewModels/SpotAdd/SpotAddViewModel.swift
+++ Tourding_FE/ViewModels/SpotAdd/SpotAddViewModel.swift
@@ -9,4 +9,25 @@ import Foundation
 
 final class SpotAddViewModel: ObservableObject {
     let tagFilter: [String] = ["전체","자연", "인문(문화/예술/역사)", "레포츠", "쇼핑", "음식", "숙박"]
+    
+    func matchImageName(for title: String)-> String {
+        switch title {
+        case "전체":
+            return "icon_menu"
+        case "자연":
+            return "icon_nature"
+        case "인문(문화/예술/역사)":
+            return "icon_humanities"
+        case "레포츠":
+            return "icon_Leports"
+        case "쇼핑":
+            return "icon_shopping"
+        case "음식":
+            return "icon_food"
+        case "숙박":
+            return "icon_Accommodation"
+        default:
+            return "icon_menu"
+        }
+    } // : func
 }
--- Tourding_FE/Views/SpotAdd/SpotAddView.swift
+++ Tourding_FE/Views/SpotAdd/SpotAddView.swift
@@ -22,6 +22,58 @@ struct SpotAddView: View {
             
             section
             
+            filterSection
+                .padding(.bottom, 24)
+            
+            ScrollView(showsIndicators: false) {
+                VStack(alignment: .leading, spacing: 0) {
+                    
+                    //Row Cell
+                    HStack(alignment: .top, spacing: 0){
+                        VStack{
+                            Image("empty")
+                        }
+                        .frame(width: 52, height: 52)
+                        .background(Color.gray1)
+                        .cornerRadius(10)
+                        
+    
+                        VStack(alignment: .leading, spacing: 2) {
+                            Text("불국사")
+                                .foregroundColor(.gray6)
+                                .font(.pretendardSemiBold(size: 16))
+                            
+                            Text("경북 경산시 조영동")
+                                .foregroundColor(.gray4)
+                                .font(.pretendardRegular(size: 14))
+                        }
+                        .padding(.leading, 12)
+                        .padding(.top, 5)
+                        
+                        Spacer()
+                        
+                        Button(action:{}){
+                            Text("추가")
+                                .foregroundColor(.gray4)
+                                .padding(.vertical, 9.2)
+                                .padding(.horizontal, 12)
+                                .background(Color.gray1)
+                                .cornerRadius(10)
+                        }
+                        .padding(.vertical, 10)
+                        
+                    } // : HStack
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 12)
+
+                    
+                } //: VStack
+                .padding(.top, 4)
+                .background(.white)
+                .cornerRadius(16)
+                .padding(.horizontal, 16)
+            } // : ScrollView
+            
             Spacer()
         } // : VStack
         .navigationBarBackButtonHidden()
@@ -75,6 +127,38 @@ struct SpotAddView: View {
                 .padding(.top, 14)
         } // : HStack
         .padding(.leading, 16)
+        .padding(.bottom, 16)
+    }
+    
+    private func toggleFilterButton(
+        title: String
+    ) -> some View {
+        Button(action:{}){
+            HStack(spacing: 0) {
+                Image(spotAddViewModel.matchImageName(for: title))
+                
+                Text(title)
+                    .foregroundColor(.gray4)
+                    .font(.pretendardMedium(size: 14))
+            }
+            .padding(.vertical, 6)
+            .padding(.leading, 10)
+            .padding(.trailing, 12)
+            .background(Color.white)
+            .cornerRadius(10)
+        }
+    }
+    
+    private var filterSection: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(alignment: .top, spacing: 8) {
+                ForEach(spotAddViewModel.tagFilter, id:\.self){ tag in
+                    toggleFilterButton(title: tag)
+                }
+            } // : HStack
+            .padding(.leading, 16)
+            .padding(.bottom, 14)
+        } // : ScrollView
     }
 }
 
