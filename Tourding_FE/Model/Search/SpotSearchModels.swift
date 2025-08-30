c247ce55fcb65a3e0bcbaccb23ae53022514a94b
YooJaeHyeok <yoojh5685@gmail.com>
2025-08-30 15:15:33 +0900

    [chore] Tour data model 생성

--- Tourding_FE/Model/Search/SpotSearchModels.swift
+++ Tourding_FE/Model/Search/SpotSearchModels.swift
@@ -7,9 +7,7 @@
 
 import Foundation
 
-// MARK: - Request Models
-
-/// 스팟 검색 요청 바디
+// MARK: - Request
 struct SpotSearchRequest: Codable {
     let pageNum: Int
     let mapX: String
@@ -17,72 +15,20 @@ struct SpotSearchRequest: Codable {
     let radius: String
 }
 
-// MARK: - Response Models
-
-/// 서버 응답 구조
-struct SpotSearchResponse: Codable {
-    let data: [SpotData]
-    let hasNext: Bool
-    let totalElements: Int
-    
-    // 서버 응답에 따라 필드명 조정 필요할 수 있음
-}
-
-/// 스팟 데이터 구조 (서버 응답에 맞게 조정)
+// MARK: - Response
 struct SpotData: Codable, Identifiable {
-    let id = UUID() // SwiftUI ForEach용
+    let id = UUID()
     let title: String
     let addr1: String
-    let contentId: String
-    let contentTypeId: String
-    let firstImage: String
-    let firstImage2: String
-    let mapX: String
-    let mapY: String
+    let contentid: String
+    let contenttypeid: String
+    let firstimage: String
+    let firstimage2: String
+    let mapx: String
+    let mapy: String
     
-    // JSON 키와 Swift 프로퍼티명이 다를 경우 CodingKeys 사용
     private enum CodingKeys: String, CodingKey {
-        case title
-        case addr1
-        case contentId = "contentid"
-        case contentTypeId = "contenttypeid"
-        case firstImage = "firstimage"
-        case firstImage2 = "firstimage2"
-        case mapX = "mapx"
-        case mapY = "mapy"
+        case title, addr1, contentid, contenttypeid
+        case firstimage, firstimage2, mapx, mapy
     }
-}
-
-// MARK: - Additional Models (필요시 추가)
-
-/// 스팟 상세 정보 응답
-struct SpotDetailResponse: Codable {
-    let data: SpotDetailData
-}
-
-/// 스팟 상세 데이터
-struct SpotDetailData: Codable {
-    let contentId: String
-    let title: String
-    let addr1: String
-    let addr2: String?
-    let tel: String?
-    let homepage: String?
-    let overview: String?
-    let firstImage: String
-    let firstImage2: String
-    let mapX: String
-    let mapY: String
-    let contentTypeId: String
-}
-
-/// 북마크 요청
-struct BookmarkRequest: Codable {
-    let contentId: String
-}
-
-/// 북마크 응답
-struct BookmarkResponse: Codable {
-    let success: Bool
-    let message: String
 }
