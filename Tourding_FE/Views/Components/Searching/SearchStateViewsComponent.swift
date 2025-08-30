3265d8d8a8363cfe097e4545930d064773b11319
YooJaeHyeok <yoojh5685@gmail.com>
2025-08-30 15:10:15 +0900

    [chore] Searching 파일 세분화

--- Tourding_FE/Views/Components/Searching/SearchStateViewsComponent.swift
+++ Tourding_FE/Views/Components/Searching/SearchStateViewsComponent.swift
@@ -1,7 +1,63 @@
 //
-//  ss.swift
+//  SearchStateViewsComponent.swift
 //  Tourding_FE
 //
 //  Created by 유재혁 on 8/22/25.
 //
 
+import SwiftUI
+
+struct SearchStateViewsComponent: View {
+    enum SearchState {
+        case empty
+        case loading
+        case noResults
+    }
+    
+    let state: SearchState
+    
+    var body: some View {
+        Group {
+            switch state {
+            case .empty:
+                EmptyStateView()
+            case .loading:
+                LoadingStateView()
+            case .noResults:
+                NoResultsView()
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+// MARK: - 빈 상태 뷰
+struct EmptyStateView: View {
+    var body: some View {
+        VStack(spacing: 12) {
+            Image("searchempty")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 338)
+        }
+    }
+}
+
+// MARK: - 로딩 상태 뷰
+struct LoadingStateView: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            DotsLoadingView()
+        }
+    }
+}
+
+// MARK: - 검색 결과 없음 뷰
+struct NoResultsView: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            Image("searchwrong")
+                .frame(width: 338)
+        }
+    }
+}
